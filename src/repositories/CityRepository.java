233a2b52b6026152231403ef2b29217f5bb5c71b
Oussama Elbourakkadi <oussama.elb1@gmail.com>
2023-03-18 18:49:11 +0100

    Migrated the cities collection from a List of cities to a Set of cities

--- src/repositories/CityRepository.java
+++ src/repositories/CityRepository.java
@@ -1,23 +1,25 @@
 package repositories;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import Models.City;
 
 public class CityRepository {
 	
-	private static List<City> cities;
+	private static Set<City> cities;
 	
 	public CityRepository() {
-		cities = new ArrayList<>();
+		cities = new HashSet<>();
 	}
 	
 	public void addCity(City city) {
 		cities.add(city);
 	}
 	
-	public List<City> getAll() {
+	public Set<City> getAll() {
 		if (cities.isEmpty()) {
 			throw new IllegalStateException();
 		}
